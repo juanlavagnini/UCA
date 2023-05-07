ca4aca4580a69eca11aa926c1ef69979eeec30f4
TriniSuarezC <106890300+TriniSuarezC@users.noreply.github.com>
2023-05-06 23:29:23 -0300

    Cliente Concurrente en Python

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 import socket
-import threading #Para poder crear procesos hijo
-import datetime
 
-#Creamos el socket
+#Creo el socket
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("El socket se creo")
-#Establecemos la conexion
-my_socket.bind(('localhost', 6667))
-print("Bind hecho")
-#Ponemos el socket en modo escucha
-my_socket.listen(1)
-print("Socket en modo escucha")
+print("Socket creado")
+#Conecto al servidor
+my_socket.connect(('localhost',6667))
+print("Conexion establecida")
 
-def proceso_hijo(*args):
-    conexion = args[0]   #El primer elemento de la lista args es el objeto de socket que representa la conexion de un cliente especifico
-    addr = args[1]       #Idem pero segundo elemento a la direccion
+contador = 0
+while contador<1:
+    #Enviamos datos
+    print("Contador: ", contador)
+    datos = my_socket.recv(1024)
+    print(datos.decode('utf-8'))
+    mensaje = 'Hola Soy el Cliente 1'
+    print("Enviando: ", mensaje)
+    my_socket.sendall(mensaje.encode())
 
-    try:
-        print("Conexion con {}.".format(addr))  #Imprimo la direccion de la conexion
-        conexion.send("Servidor: Conectado con cliente".encode('UTF-8'))
-        inicio = datetime.datetime.now()
-        mensajeHoy = "Conexion del cliente a las " + inicio.strftime("%Y-%m-%d  %H:%M:%S")
-        conexion.send(mensajeHoy.encode('UTF-8'))
+    bytes_recibidos = 0
+    bytes_esperados = len(mensaje)
 
-        while True:
-            data = conexion.recv(1024)
-            print("Recibido: ", addr)
-            if data:
-                print("Enviando mensaje de vuelta al cliente")
-                conexion.sendall(data)
-            else:
-                print("No hay mas datos", addr)
-                break
-            fin = datetime.datetime.now()
-            duracion = fin - inicio
-            segundos = duracion.total_seconds()
-            mensajeFin = "La conexion duro: " + str(segundos) + " segundos."
-            conexion.send(mensajeFin.encode('UTF-8'))
-    finally:
-        conexion.close()
+    fecha = my_socket.recv(2048)
+    print("Fecha: ", fecha.decode('UTF-8'))
 
-while 1:
-    conexion, addr = my_socket.accept()
-    print("Data del cliente", addr)
-    threading.Thread(target=proceso_hijo, args=(conexion, addr)).start()
+    while bytes_recibidos<bytes_esperados:
+        data = my_socket.recv(2048)
+        bytes_recibidos += len(data)
+        print("Recibiendo del servidor: ", data)
 
-#mensajeHoy = "Conexion del cliente a las " + diaHora.strftime("%Y-%m-%d  %H:%M:%S")
-#conexion.send(mensajeHoy.encode())
+    tiempo = my_socket.recv(1024)
+    print("Tiempo: ", tiempo.decode('UTF-8'))
+
+    contador = contador+1
+
+print("Cerrando socket")
+my_socket.close()
+
+
